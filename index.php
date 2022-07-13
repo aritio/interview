b8e8f2cadc3775316bb04492d1a68e206c70411b
ardizariat <ardizariat@gmail.com>
2022-07-13 13:37:46 +0700

    ardi nor dzariat

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 require_once('./VendingMachine.php');
 
-
 $makanan = new VendingMachine();
 /* 
 Instansiasi beli makanan dengan 3 parameter, 
 nama makanan(str), uang yang dimasukan(int), jumlah makanan(int) 
 */
-$beliMakanan = $makanan->beliMakanan('biskuit', 50000, 5);
+$beliMakanan = $makanan->beliMakanan('chips', 20000, 2);
 print_r($beliMakanan);
